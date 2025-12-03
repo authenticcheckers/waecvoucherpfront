1f19187691c86601d95e8e007fc0f8de28d54029
authenticcheckers <authenticcheckers@gmail.com>
2025-12-03 23:25:34 +0000

    Create Footer.tsx

---
+++ app/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white py-6 mt-10 text-center">
+      © 2025 Waec e-Vouchers Hub. All Rights Reserved.
+    </footer>
+  );
+}
