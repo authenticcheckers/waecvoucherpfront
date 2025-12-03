a7ff5f0aa777f563829b302ab797810a842a1dcd
authenticcheckers <authenticcheckers@gmail.com>
2025-12-03 23:46:30 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "Waec e-Vouchers Hub",
-  description: "Purchase WAEC vouchers instantly",
+  title: "WAEC E-Voucher Portal",
+  description: "Purchase WAEC BECE & SHS school placement checker vouchers",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>{children}</body>
